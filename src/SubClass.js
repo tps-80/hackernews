358ead1ce80612f80cc802af2d4d4d16b38e4aef
Thomas Sorensen <12405751+tps-80@users.noreply.github.com>
2018-10-10 09:36:48 -0700

    Refactored to ES6

---
+++ src/SubClass.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class SubClass extends Component {
+  render() {
+    return (
+      <h3> SubClass as a classy thing </h3>
+    );
+  }
+}
+
+export default SubClass;
