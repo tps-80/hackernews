add9f894b9e0b41b984fce06b368a8679c1aa607
Thomas Sorensen <12405751+tps-80@users.noreply.github.com>
2018-10-11 10:46:18 -0700

    Added conditional redering

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ class App extends Component {
     const isNotId = item => item.objectID !== id;
     const updatedHits = this.state.result.hits.filter(isNotId);
     this.setState({
-      result: Object.assign({}, this.state.result, { hits: updatedHits })
+      result: {...this.state.result, hits: updatedHits }
     });
   }
 
@@ -71,11 +71,14 @@ class App extends Component {
             Search
           </Search>
         </div>
-        <Table
-          list={result.hits}
-          pattern={searchTerm}
-          onDismiss={this.onDismiss}
-        />
+        { result
+          ?  <Table
+            list={result.hits}
+            pattern={searchTerm}
+            onDismiss={this.onDismiss}
+            />
+            : null
+        }
       </div>
     );
   }
