24d7be3ef158d4e0097bde6c2cd0dc7f735d7596
Thomas Sorensen <12405751+tps-80@users.noreply.github.com>
2018-10-16 12:24:38 -0700

    trying some things

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
 const DEFAULT_HPP = '100';
-const PATH_BASE = 'https://hn.algolia.com/api/v13';
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
 const PARAM_PAGE = 'page=';
@@ -20,6 +21,7 @@ const smallColumn = {
 };
 
 class App extends Component {
+  _isMounted = false;
 
   constructor(props) {
     super(props);
@@ -61,16 +63,20 @@ class App extends Component {
   }
 
   fetchSearchTopStories(searchTerm, page = 0) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
-      .then(response => response.json())
-      .then(result => this.setSearchTopStories(result))
-      .catch(error => this.setState({ error }));
+    axios(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+      .then(result => this._isMounted && this.setSearchTopStories(result.data))
+      .catch(error => this._isMounted && this.setState({ error }));
   }
 
   componentDidMount() {
+    this._isMounted = true;
     const { searchTerm } = this.state;
     this.setState({ searchKey: searchTerm });
     this.fetchSearchTopStories(searchTerm);
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false;
   }
 
   onSearchChange(event) {
@@ -119,7 +125,7 @@ class App extends Component {
         results[searchKey].hits
     ) || [];
 
-    if (error != null) {
+    if (error) {
       console.log("error fired")
       return <p>Something went wrong.</p>;
     }
@@ -213,3 +219,9 @@ const Table = ({ list, onDismiss}) =>
   </div>
 
 export default App;
+
+export {
+  Button,
+  Search,
+  Table,
+};
