9c8d65a8988d7942d2e0ba37b00adcdcf6865d65
Thomas Sorensen <12405751+tps-80@users.noreply.github.com>
2018-11-01 22:42:28 -0700

    Changed update object to function and updated snapshot

--- src/App.js
+++ src/App.js
@@ -38,6 +38,24 @@ const SORTS = {
   POINTS: list => sortBy(list, 'points').reverse(),
 }
 
+const updateSearchTopStoriesState = (hits, page) => (prevState) => {
+  const { searchKey, results } = prevState;
+  const oldHits = results && results[searchKey]
+    ? results[searchKey].hits
+    : [];
+  const updatedHits = [
+    ...oldHits,
+    ...hits
+  ];
+  return {
+    results: {
+      ...results,
+      [searchKey]: { hits: updatedHits, page }
+    },
+    isLoading: false
+  };
+}
+
 class App extends Component {
   _isMounted = false;
 
@@ -66,25 +84,9 @@ class App extends Component {
 
   setSearchTopStories(result) {
     const { hits, page } = result;
-
-    this.setState(prevState => {
-      const { searchKey, results } = prevState;
-      const oldHits = results && results[searchKey]
-        ? results[searchKey].hits
-        : [];
-      const updatedHits = [
-        ...oldHits,
-        ...hits
-      ];
-      return {
-        results: {
-          ...results,
-          [searchKey]: { hits: updatedHits, page }
-        },
-        isLoading: false
-      };
-    });
+    this.setState(updateSearchTopStoriesState(hits,page));
   }
+
 
   fetchSearchTopStories(searchTerm, page = 0) {
     this.setState({ isLoading: true });
